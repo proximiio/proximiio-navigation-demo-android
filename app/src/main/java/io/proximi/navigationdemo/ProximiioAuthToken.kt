8851708f4b2dd83099682ebcf2d580ab500a307b
Matteo Crippa <matteocrippa@users.noreply.github.com>
2022-01-27 19:57:59 +0100

    fixes

--- app/src/main/java/io/proximi/navigationdemo/ProximiioAuthToken.kt
+++ app/src/main/java/io/proximi/navigationdemo/ProximiioAuthToken.kt
@@ -4,6 +4,5 @@ package io.proximi.navigationdemo
  * TODO: Set your own Proximi.io authentication token here.
  */
 object ProximiioAuthToken {
-    val TOKEN: String =
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImlzcyI6ImMxZWFhYjFhLTNmMDItNDQ5MS1hNTE1LWFmOGQ2MjhmNzRmYiIsInR5cGUiOiJhcHBsaWNhdGlvbiIsImFwcGxpY2F0aW9uX2lkIjoiYzQxNjMyZDgtZDIxNi00MTMwLTg1ZmYtNTJmYTdhMzE0OWI1In0.FEBlAxIamCJNi7XF4T2RO5oXJ0mLe-1Bee-qBwUMj_4"
+    val TOKEN: String = "<TOKEN>"
 }
